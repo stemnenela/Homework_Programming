be5d0c44dad78cf1be940616668d1fa7406019f8
stemnenela <nenelamathie0@gmail.com>
2024-10-18 15:57:36 -0400

    10\18

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
 
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<rigidbody2D>();
+
+    }
 }
+        
